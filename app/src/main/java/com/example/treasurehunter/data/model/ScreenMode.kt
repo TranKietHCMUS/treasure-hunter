15adbf1ffa8f7a717fbc3dc342991f48eb50a144
fusodoya <fusodoya@gmail.com>
2025-01-13 13:42:10 +0700

    add InGameViewModel, controll game screen mode

---
+++ app/src/main/java/com/example/treasurehunter/data/model/ScreenMode.kt
@@ -0,0 +1,7 @@
+package com.example.treasurehunter.data.model
+
+enum class ScreenMode {
+    MAP,
+    AR,
+    PUZZLE,
+}
--- app/src/main/java/com/example/treasurehunter/ui/screen/InGameScreen.kt
+++ app/src/main/java/com/example/treasurehunter/ui/screen/InGameScreen.kt
@@ -1,13 +1,8 @@
 package com.example.treasurehunter.ui.screen
 
 import MapScreen
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Home
@@ -18,46 +13,38 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.treasurehunter.LocalNavController
+import com.example.treasurehunter.data.model.ScreenMode
+import com.example.treasurehunter.data.viewModel.InGameViewModel
 
 data class NavItem(
     val label : String,
     val icon : ImageVector,
+    val mode : ScreenMode
 )
 
 @Preview
 @Composable
 fun InGameScreen() {
     val navItemList = listOf(
-        NavItem("Map", Icons.Default.Home),
-        NavItem("AR", Icons.Filled.Person),
-        NavItem("Test", Icons.Filled.Create),
+        NavItem("Map", Icons.Default.Home, ScreenMode.MAP),
+        NavItem("AR", Icons.Filled.Person, ScreenMode.AR),
+        NavItem("Puzzle", Icons.Filled.Create, ScreenMode.PUZZLE),
     )
-
-    var selectedIndex by remember { mutableStateOf(0) }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
             NavigationBar {
-                navItemList.forEachIndexed { index, navItem ->
+                navItemList.forEach { navItem ->
                     NavigationBarItem(
                         icon = { Icon(navItem.icon, contentDescription = "Icon") },
                         label = { Text(navItem.label) },
-                        selected = index == 0,
+                        selected = (navItem.mode == InGameViewModel.screenMode),
                         onClick = {
-                            selectedIndex = index
+                            InGameViewModel.changeScreenMode(navItem.mode)
                         }
                     )
                 }
@@ -67,7 +54,6 @@ fun InGameScreen() {
         innerPadding ->
         ContentScreen(
             modifier = Modifier.padding(innerPadding),
-            selectedIndex = selectedIndex
         )
     }
 
@@ -75,10 +61,10 @@ fun InGameScreen() {
 }
 
 @Composable
-fun ContentScreen(modifier: Modifier, selectedIndex: Int) {
-    when (selectedIndex) {
-        0 -> MapScreen(modifier)
-        1 -> ARScreen()
-        2 -> TestScreen()
+fun ContentScreen(modifier: Modifier) {
+    when (InGameViewModel.screenMode) {
+        ScreenMode.MAP -> MapScreen(modifier)
+        ScreenMode.AR -> ARScreen()
+        ScreenMode.PUZZLE -> TestScreen()
     }
 }
