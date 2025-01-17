3ff4b8ec506c7920f29d01697b80107d5a697934
fusodoya <fusodoya@gmail.com>
2025-01-17 18:43:39 +0700

    change color

--- app/src/main/java/com/example/treasurehunter/ui/component/BottomNavigate.kt
+++ app/src/main/java/com/example/treasurehunter/ui/component/BottomNavigate.kt
@@ -1,5 +1,6 @@
 package com.example.treasurehunter.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,10 +12,13 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
@@ -24,6 +28,7 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.treasurehunter.data.model.ScreenMode
 import com.example.treasurehunter.data.viewModel.GameViewModel
 import com.example.treasurehunter.R
+import com.example.treasurehunter.ui.theme.Orange
 
 data class NavItem(
     val label : String,
@@ -41,8 +46,10 @@ fun BottomNavigate() {
         NavItem("Puzzle", lottieResId = R.raw.puzzle_animation, mode = ScreenMode.PUZZLE, size = 50, padding = 0),
     )
 
-    NavigationBar (
-        modifier = Modifier.padding(0.dp),
+    NavigationBar(
+        modifier = Modifier
+            .padding(0.dp)
+            .background(Orange), // Màu nâu
     ) {
         navItemList.forEach { navItem ->
             val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(navItem.lottieResId ?: 0))
@@ -57,19 +64,28 @@ fun BottomNavigate() {
                     LottieAnimation(
                         composition,
                         progress,
-                        modifier = Modifier.size(navItem.size.dp).padding(navItem.padding.dp)
+                        modifier = Modifier
+                            .size(navItem.size.dp)
+                            .padding(navItem.padding.dp),
                     )
-                       },
+                },
                 label = {
                     Text(
                         navItem.label,
+                        color = Color.Black, // Đặt màu chữ trắng
                         modifier = Modifier.padding(0.dp)
                     )
-                        },
+                },
                 selected = (navItem.mode == GameViewModel.screenMode),
                 onClick = {
                     GameViewModel.changeScreenMode(navItem.mode)
-                }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.White, // Màu icon khi được chọn
+                    unselectedIconColor = Color.White, // Màu icon khi không được chọn
+                    selectedTextColor = Color.White, // Màu chữ khi được chọn
+                    unselectedTextColor = Color.White // Màu chữ khi không được chọn
+                )
             )
         }
     }
